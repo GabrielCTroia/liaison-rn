60f032c4219c5abe70f39a8cd3ba1f77dd3bfe57
Gabriel C. Troia <catalin.troia@gmail.com>
2019-08-03 12:47:05 -0400

    Install redux-thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './rootReducerasd';
+import thunk from 'redux-thunk';
+
+let middlewares = [
+  applyMiddleware(thunk),
+];
+
+if (typeof (window as any) === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION__) {
+  middlewares = middlewares.concat((window as any).__REDUX_DEVTOOLS_EXTENSION__({
+    serialize: {
+      options: true,
+    }
+  }));
+}
 
 export const store = createStore(
   rootReducer,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-)
+  {},
+  compose(...middlewares),
+);
